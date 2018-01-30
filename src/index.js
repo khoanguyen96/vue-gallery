0139aeb190b61d334a63e00e64f250ea7c48227b
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-30 12:06:21 -0800

    make this work for browsers as well

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import VueGallery from './component/gallery.vue';
 
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.component('vue-gallery', VueGallery);
+}
+
 export default VueGallery;
